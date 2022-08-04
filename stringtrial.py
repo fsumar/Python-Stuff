a1c53995de917c9059781d4722d191d9bf7149d6
Farhan S <fsumar@hotmail.co.uk>
2022-08-04 14:56:50 +0100

    mine

---
+++ stringtrial.py
@@ -0,0 +1,15 @@
+strings = "hello"
+whole = 20
+floats = 7.0
+
+if strings == "hello":
+    print('corect')
+
+if isinstance(strings, str):
+    print('even correct')
+
+if isinstance (whole, int) and whole == 20:
+    print('well done')
+if isinstance (floats, float) and floats == 7.0:
+    print('good') 
+    print(floats)
